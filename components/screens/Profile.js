f5f9a77b2920c6b98b440488ade7434772d2aa94
Corvu-s <roomtemp72@gmail.com>
2020-07-14 16:07:13 -0400

    messing with cards in the profile page

--- components/screens/Profile.js
+++ components/screens/Profile.js
@@ -1,10 +1,24 @@
 import React from "react";
-import { StyleSheet, View, Text, Button } from "react-native";
+import { StyleSheet, View, Text } from "react-native";
 
+import { Avatar, Button, Card, Title, Paragraph } from "react-native-paper";
 function PROFILE({ navigation }) {
   return (
     <View style={styles.container}>
       <Text>PROFILE</Text>
+
+      <Card>
+        <Card.Title title="Card Title" subtitle="Card Subtitle" />
+        <Card.Content>
+          <Title>Card title</Title>
+          <Paragraph>Card content</Paragraph>
+        </Card.Content>
+        <Card.Cover source={require("../images/test.png")} />
+        <Card.Actions>
+          <Button>Cancel</Button>
+          <Button>Ok</Button>
+        </Card.Actions>
+      </Card>
     </View>
   );
 }
@@ -15,7 +29,10 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+  },
+  avatar: {
+    position: "absolute",
+    top: 100,
+    left: 50,
   },
 });

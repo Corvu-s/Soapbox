6e351f9e6a4115737a114982010f71f8f65d6bc4
Corvu-s <roomtemp72@gmail.com>
2020-07-14 22:20:20 -0400

    started prototype 4 design

--- components/screens/Profile.js
+++ components/screens/Profile.js
@@ -11,27 +11,22 @@ import {
 } from "react-native-paper";
 function PROFILE({ navigation }) {
   return (
-    <View style={styles.container}>
-      <Text>PROFILE</Text>
+    <>
+      <View style={styles.container}>
+        <View style={styles.BannerLowerShadow}></View>
+        <View style={styles.BannerUpperShadow}>
+          <Text style={styles.name}>Spongebob</Text>
+          <Text style={styles.handle}>@spongboi</Text>
+          <Text style={styles.tweetNum}>42069 tweets</Text>
 
-      {/* <Card>
-        <Card.Title title="Card Title" subtitle="Card Subtitle" />
-        <Card.Content>
-          <Title>Card title</Title>
-          <Paragraph>Card content</Paragraph>
-        </Card.Content>
-        <Card.Cover source={require("../images/test.png")} />
-        <Card.Actions>
-          <Button>Cancel</Button>
-          <Button>Ok</Button>
-        </Card.Actions>
-      </Card> */}
-      <View style={styles.BannerLowerShadow}></View>
-      <View style={styles.BannerUpperShadow}>
-        <Text>*Breath in</Text>
-        <Avatar.Image size={100} source={require("../images/test.png")} />
+          <Avatar.Image
+            size={100}
+            source={require("../images/test.png")}
+            style={styles.avatar}
+          />
+        </View>
       </View>
-    </View>
+    </>
   );
 }
 
@@ -39,20 +34,45 @@ export default PROFILE;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+    position: "absolute",
+    top: 551,
+    left: 0,
+    height: 270,
+    width: 375,
+    borderRadius: 20,
     backgroundColor: "#2B2B2B",
   },
   avatar: {
     position: "absolute",
-    top: 100,
-    left: 50,
+    left: 180,
+  },
+  name: {
+    fontSize: 30,
+    color: "white",
+    top: 10,
+    left: 10,
+    position: "absolute",
+  },
+  tweetNum: {
+    position: "absolute",
+    fontSize: 11,
+    color: "white",
+    top: 80,
+    left: 10,
+  },
+  handle: {
+    position: "absolute",
+    fontSize: 20,
+    color: "grey",
+    top: 50,
+    left: 10,
   },
   BannerUpperShadow: {
     borderRadius: 20,
     shadowColor: "#3A3A3A",
     shadowOffset: {
       width: -5,
-      height: -7,
+      height: -10,
     },
     shadowOpacity: 1,
     shadowRadius: 13,
@@ -60,10 +80,10 @@ const styles = StyleSheet.create({
 
     ////////////////////postion,not to do with shadow
     position: "absolute",
-    top: 100,
-    left: 15,
-    height: 200,
-    width: 350,
+    top: 50,
+    left: 40,
+    height: 160,
+    width: 294,
     flex: 1,
     justifyContent: "center",
     backgroundColor: "#2B2B2B",
@@ -73,17 +93,17 @@ const styles = StyleSheet.create({
     shadowColor: "#222222",
     shadowOffset: {
       width: 10,
-      height: 7,
+      height: 10,
     },
     shadowOpacity: 1,
     shadowRadius: 5,
     elevation: 13,
     ///////////////////
     position: "absolute",
-    top: 100,
-    left: 15,
-    height: 200,
-    width: 350,
+    top: 50,
+    left: 40,
+    height: 160,
+    width: 294,
     flex: 1,
     justifyContent: "center",
     backgroundColor: "#2B2B2B",
